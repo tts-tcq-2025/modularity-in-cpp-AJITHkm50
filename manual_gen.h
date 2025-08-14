c3b28ec34c4fcedbf1e541aa43ccc5196c7bcca6
AJITHkm50 <fjt2kor@bosch.com>
2025-08-14 10:47:29 +0530

    Update and rename ColorManual.h to manual_gen.h

---
+++ manual_gen.h
@@ -0,0 +1,9 @@
+#ifndef MANUAL_GEN_H
+#define MANUAL_GEN_H
+#include <iostream> // For std::ostream
+namespace TelCo { // Namespace must match
+// Function to print the color code manual
+// os defaults to std::cout
+void print_manual(std::ostream& os = std::cout);
+} // namespace TelCo
+#endif // MANUAL_GEN_H
