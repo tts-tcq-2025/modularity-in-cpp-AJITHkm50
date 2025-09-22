9efbc1a060fdd6397bc46cc76e0030545591aad9
AJITHkm50 <fjt2kor@bosch.com>
2025-09-22 19:11:33 +0530

    Create ColorManual.h

---
+++ ColorManual.h
@@ -0,0 +1,9 @@
+#ifndef COLOR_MANUAL_H
+#define COLOR_MANUAL_H
+#include <ostream>
+namespace TelCoColorCoder {
+namespace ColorManual {
+void PrintColorCodeManual(std::ostream& os);
+}
+}
+#endif // COLOR_MANUAL_H
