7c4b1cdafeb9de325217eb2ce3d895bf7cc81349
AJITHkm50 <fjt2kor@bosch.com>
2025-09-22 19:12:03 +0530

    Create ColorManual.cpp

---
+++ ColorManual.cpp
@@ -0,0 +1,9 @@
+#include "ColorManual.h"
+#include "manual_gen.h"
+namespace TelCoColorCoder {
+namespace ColorManual {
+void PrintColorCodeManual(std::ostream& os) {
+    TelCo::print_manual(os);
+}
+}
+}
